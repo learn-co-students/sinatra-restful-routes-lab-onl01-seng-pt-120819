0348e0a65c5ca9f350163ab1021510efe0245617
dwane richards <dwanerich@gmail.com>
2020-04-01 01:56:35 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,46 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # code actions here!
-
+  get "/recipes/new" do
+    erb :new
+  end
+  
+  get '/recipes' do
+    recipes = Recipe.all
+    erb :index, locals: { recipes: recipes }
+  end
+  
+  get '/recipes/:id' do
+    recipe = Recipe.find(params[:id])
+    erb :show, locals: { recipe: recipe }
+  end
+  
+  get '/recipes/:id/edit' do
+    recipe = Recipe.find(params[:id])
+    erb :edit, locals: { recipe: recipe } 
+  end
+  
+  post '/recipes' do
+    recipe = Recipe.create(params)
+    
+    redirect to "/recipes/#{recipe.id}"
+  end
+  
+  patch '/recipes/:id' do
+    recipe = Recipe.find(params[:id])
+    
+    
+    recipe.name = params[:name]
+    recipe.cook_time = params[:cook_time]
+    recipe.ingredients = params[:ingredients]
+    
+    recipe.save
+    redirect to "/recipes/#{recipe.id}"
+  end
+  
+  delete '/recipes/:id/delete' do
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.delete
+    redirect to "/recipes"
+  end
 end
--- spec/sinatra_restful_routes_lab_spec.rb
+++ spec/sinatra_restful_routes_lab_spec.rb
@@ -156,6 +156,7 @@ describe "Recipe App" do
     end
 
     it "updates the recipe" do
+      
       expect(page).to have_content("Double chocolate chip cookies")
       expect(page).to have_content("chocolate chips, flour, sugar, butter, cocoa powder")
       expect(page).to have_content("30 minutes")
@@ -177,11 +178,13 @@ describe "Recipe App" do
       )
       visit  "/recipes/#{@cookie.id}"
 
-      click_button "delete"
     end
 
     it "deletes a recipe" do
-      expect(Recipe.find_by_id(@cookie.id)).to eq(nil)
+      expect {
+        click_button "delete"
+      }.to change(Recipe, :count).by -1
+
     end
 
   end
